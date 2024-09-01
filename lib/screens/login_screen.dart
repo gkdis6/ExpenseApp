424de946290bf30701a95bb71a0d956b82454298
SangYong <gkdis6@gmail.com>
2024-09-01 13:38:50 +0900

    feature auto login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:financial_app/supabase.dart';
 import 'package:financial_app/screens/budget_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -13,6 +14,26 @@ class _LoginScreenState extends State<LoginScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final SupabaseClient _supabase = SupabaseClientInstance.client;
+  bool _isAutoLoginChecked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAutoLogin();
+  }
+
+  Future<void> _checkAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedEmail = prefs.getString('email');
+    final savedPassword = prefs.getString('password');
+
+    if (savedEmail != null && savedPassword != null) {
+      // 자동 로그인 시도
+      _emailController.text = savedEmail;
+      _passwordController.text = savedPassword;
+      _login();
+    }
+  }
 
   Future<void> _login() async {
     try {
@@ -22,13 +43,16 @@ class _LoginScreenState extends State<LoginScreen> {
       );
 
       if (response.session != null) {
-        // 로그인 성공 시 대시보드로 이동
+        if (_isAutoLoginChecked) {
+          final prefs = await SharedPreferences.getInstance();
+          await prefs.setString('email', _emailController.text);
+          await prefs.setString('password', _passwordController.text);
+        }
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => BudgetScreen()),
         );
       } else {
-        // 로그인 실패 시 에러 메시지 표시
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('로그인 실패: 로그인에 실패했습니다.')),
         );
@@ -49,6 +73,10 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Future<void> _logout() async {
     try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove('email');
+      await prefs.remove('password');
+
       await _supabase.auth.signOut();
       Navigator.pushReplacement(
         context,
@@ -87,6 +115,15 @@ class _LoginScreenState extends State<LoginScreen> {
               obscureText: true,
             ),
             SizedBox(height: 16),
+            CheckboxListTile(
+              title: Text('자동 로그인'),
+              value: _isAutoLoginChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  _isAutoLoginChecked = value ?? false;
+                });
+              },
+            ),
             ElevatedButton(
               onPressed: _login,
               child: Text('로그인'),
