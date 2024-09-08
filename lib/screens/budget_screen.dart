d8db7d127aa2436601a219ea5e7d9e3c9409a3a2
SangYong <gkdis6@gmail.com>
2024-09-08 23:35:27 +0900

    feat 로그아웃 아이콘 추가

--- lib/screens/budget_screen.dart
+++ lib/screens/budget_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:financial_app/screens/chart_view.dart';
 import 'package:financial_app/screens/calendar_view.dart';
+import 'package:financial_app/utils/auth.dart';
 
 class BudgetScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.logout),
+          onPressed: () => logout(context),
+        ),
         title: Text("Budget Manager"),
         actions: [
           IconButton(
