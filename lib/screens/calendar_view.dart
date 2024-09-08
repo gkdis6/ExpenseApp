4489bbf3e5a8afd8323b24882fbd5c5846863f6e
SangYong <gkdis6@gmail.com>
2024-09-08 23:37:57 +0900

    feat 캘린더 뷰 등록된 금액 있을 경우 해당 날짜 아래에 dot 표현

--- lib/screens/calendar_view.dart
+++ lib/screens/calendar_view.dart
@@ -14,14 +14,21 @@ class _CalendarViewState extends State<CalendarView> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
   List<dynamic> _selectedDayTransactions = [];
+  Map<DateTime, List<dynamic>> _events = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMonthlyTransactions(_focusedDay);
+  }
 
   Future<void> _fetchTransactionsForDay(DateTime date) async {
     final response = await _supabase
         .from('transaction')
         .select()
-        .eq('date', date.toIso8601String().substring(0, 10)); // YYYY-MM-DD
+        .eq('date', date.toIso8601String().substring(0, 10));
 
-    if (!response.isEmpty) {
+    if (response != null && response.isNotEmpty) {
       setState(() {
         _selectedDayTransactions = response;
       });
@@ -30,6 +37,43 @@ class _CalendarViewState extends State<CalendarView> {
         _selectedDayTransactions = [];
       });
     }
+  }
+
+  Future<void> _fetchMonthlyTransactions(DateTime focusedDay) async {
+    final firstDayOfMonth = DateTime(focusedDay.year, focusedDay.month, 1);
+    final lastDayOfMonth = DateTime(focusedDay.year, focusedDay.month + 1, 0);
+
+    final response = await _supabase
+        .from('transaction')
+        .select()
+        .gte('date', firstDayOfMonth.toIso8601String().substring(0, 10))
+        .lt('date', lastDayOfMonth.toIso8601String().substring(0, 10));
+
+    if (response != null && response.isNotEmpty) {
+      Map<DateTime, List<dynamic>> events = {};
+
+      for (var transaction in response) {
+        final date = DateTime.parse(transaction['date']);
+        final dateWithoutTime = DateTime(date.year, date.month, date.day);
+        if (events[dateWithoutTime] == null) {
+          events[dateWithoutTime] = [];
+        }
+        events[dateWithoutTime]!.add(transaction);
+      }
+
+      setState(() {
+        _events = events;
+      });
+    } else {
+      setState(() {
+        _events = {}; // 데이터가 없을 경우 이벤트 맵 초기화
+      });
+    }
+  }
+
+  List<dynamic> _getEventsForDay(DateTime day) {
+    final dateWithoutTime = DateTime(day.year, day.month, day.day);
+    return _events[dateWithoutTime] ?? [];
   }
 
   @override
@@ -64,7 +108,20 @@ class _CalendarViewState extends State<CalendarView> {
             },
             onPageChanged: (focusedDay) {
               _focusedDay = focusedDay;
+              _fetchMonthlyTransactions(focusedDay);
             },
+            eventLoader: _getEventsForDay,
+            calendarBuilders: CalendarBuilders(
+              markerBuilder: (context, date, events) {
+                if (events.isNotEmpty) {
+                  return Align(
+                    alignment: Alignment.bottomCenter,
+                    child: _buildEventsMarker(events),
+                  );
+                }
+                return SizedBox();
+              },
+            ),
           ),
           Expanded(
             child: _selectedDayTransactions.isEmpty
@@ -81,6 +138,17 @@ class _CalendarViewState extends State<CalendarView> {
                   ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildEventsMarker(List events) {
+    return Container(
+      width: 7,
+      height: 7,
+      decoration: BoxDecoration(
+        color: Colors.blueAccent,
+        shape: BoxShape.circle,
       ),
     );
   }
