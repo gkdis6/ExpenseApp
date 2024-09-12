d3fe2c9a6900929b939a3815bdc6d5e8da6cf85d
SangYong <gkdis6@gmail.com>
2024-09-12 21:46:07 +0900

    feat budget 화면 하단 네이게이터로 연결

--- lib/screens/budget_screen.dart
+++ lib/screens/budget_screen.dart
@@ -3,7 +3,20 @@ import 'package:financial_app/screens/chart_view.dart';
 import 'package:financial_app/screens/calendar_view.dart';
 import 'package:financial_app/utils/auth.dart';
 
-class BudgetScreen extends StatelessWidget {
+class BudgetScreen extends StatefulWidget {
+  @override
+  State<BudgetScreen> createState() => _BudgetScreenState();
+}
+
+class _BudgetScreenState extends State<BudgetScreen> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,31 +25,50 @@ class BudgetScreen extends StatelessWidget {
           icon: Icon(Icons.logout),
           onPressed: () => logout(context),
         ),
-        title: Text("Budget Manager"),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.bar_chart),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ChartView()),
-              );
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.calendar_today),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => CalendarView()),
-              );
-            },
-          ),
-        ],
+        // title: Text("Budget Manager"),
+        // actions: [
+        //   IconButton(
+        //     icon: Icon(Icons.bar_chart),
+        //     onPressed: () {
+        //       Navigator.push(
+        //         context,
+        //         MaterialPageRoute(builder: (context) => ChartView()),
+        //       );
+        //     },
+        //   ),
+        //   IconButton(
+        //     icon: Icon(Icons.calendar_today),
+        //     onPressed: () {
+        //       Navigator.push(
+        //         context,
+        //         MaterialPageRoute(builder: (context) => CalendarView()),
+        //       );
+        //     },
+        //   ),
+        // ],
       ),
-      body: Center(
-        child: Text("Welcome to Budget Manager!"),
+      body: SwitchBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month), label: 'Calendar'),
+          BottomNavigationBarItem(icon: Icon(Icons.insights), label: 'Chart')
+        ],
+        currentIndex: index,
+        onTap: (newIndex) => setState(() {
+          index = newIndex;
+        }),
       ),
     );
+  }
+
+  Widget SwitchBody() {
+    switch (index) {
+      case 1:
+        return ChartView();
+      case 0:
+      default:
+        return CalendarView();
+    }
   }
 }
